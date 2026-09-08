79060b2e691fb0d98c0b48a048d9aa364f69e378
WCLAB <WCLAB@SVV.local>
2026-09-08 04:22:56 -0700

    Test automatic Jenkins build

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,5 +2,6 @@ public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub and Jenkins!");
         System.out.println("Continuous Integration is working!");
+        System.out.println("Continuous Integration is working - Build 2!");
     }
 }
